30cb93b67b252f7e9e5807cdc17bd8539185c82b
CarterAdamson <cpa18@humboldt.edu>
2024-03-24 16:55:54 -0700

    solved ln(0) problem in catchCurve with Adelaide's help

--- Lab 09 - Catch curve and mortality/HW9.R
+++ Lab 09 - Catch curve and mortality/HW9.R
@@ -2,8 +2,9 @@ setwd("~/Dropbox/Grad/FISH 558/Labs/Lab 09 - Catch curve and mortality")
 library(ggplot2)
 library(FSAdata)
 library(FSA)
+library(tidyverse)
 
-# Question 2 ####
+# Questitidyverse# Questitidyverse# Question 2 ####
 
 #a - choose dataset
 data("FHCatfishATL")
@@ -17,15 +18,18 @@ for(i in 1:length(cat$number)){
   }
 }
 }
+#zeroes ruin the catchCurve calculations
+cat.short <- cat %>% slice(1:15)
 
 #b - Z estimates using basic regression and Chap-Rob method
-cc.basic <- catchCurve(number~age,data=cat,ages2use=4:14) #peak occurs at age 4, age 18 outlier must be excluded b/c 0s break it
+cc.basic <- catchCurve(number~age,data=cat.short,ages2use=4:14) #peak occurs at age 4, age 18 outlier must be excluded b/c 0s break it
 summary(cc.basic)
+confint(cc.basic)
 cbind(Est=coef(cc.basic),confint(cc.basic))
 plot(cc.basic, main="Basic Catch Curve")
 
-cc1.chaprob <- chapmanRobson(number~age,data=CA,ages2use=4:10)
-summary(cc1.chapman)
-cbind(Est=coef(cc1.chapman),confint(cc1.chapman))
+cc.chaprob <- chapmanRobson(number~age,data=cat,ages2use=4:17)
+summary(cc.chaprob)
+cbind(Est=coef(cc.chaprob),confint(cc.chaprob))
 par(mar=c(5.1,4.1,4.1,2.1)) #reset figure margins, b/c otherwise can lead to errors with the plot function below...
-plot(cc1.chapman, main="Chapman Robson method")
+plot(cc.chaprob, main="Chapman Robson method")
