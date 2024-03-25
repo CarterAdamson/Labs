1a33e5c704474585f344f5b3d881e6a6f2ce128c
CarterAdamson <cpa18@humboldt.edu>
2024-03-25 09:54:21 -0700

    Morning homeworld -- all Questions have working code. Only need to finish document.

--- Lab 09 - Catch curve and mortality/HW9.R
+++ Lab 09 - Catch curve and mortality/HW9.R
@@ -4,7 +4,7 @@ library(FSAdata)
 library(FSA)
 library(tidyverse)
 
-# Questitidyverse# Questitidyverse# Question 2 ####
+# Question 2 ####
 
 #a - choose dataset
 data("FHCatfishATL")
@@ -33,3 +33,55 @@ summary(cc.chaprob)
 cbind(Est=coef(cc.chaprob),confint(cc.chaprob))
 par(mar=c(5.1,4.1,4.1,2.1)) #reset figure margins, b/c otherwise can lead to errors with the plot function below...
 plot(cc.chaprob, main="Chapman Robson method")
+
+
+# Question 3 ####
+
+#b - M and v table
+#data: threespine stickleback, Kiel Bay, Germany
+Linf <- 6.9  #from von bertalanffy model
+K <- 1.79     #k from von bertalanffy model
+t0 <- -0.28    #t0 from the von bertalanffy model
+tmax <- t0+3/K  # The maximum age for the population of fish
+T <- 8.2
+name <- c("HoenigNLS","PaulyLNoT", "PaulyL", "HoenigO","HoenigLM","HewittHoenig","tmax1",
+          "K1", "JensenK1","AlversonCarney")
+M <- c(metaM("HoenigNLS", tmax=tmax), metaM("PaulyLNoT", K=K, Linf=Linf), metaM("PaulyL", Linf=Linf, K=K, T=T),
+       metaM("HoenigO", tmax=tmax), metaM("HoenigLM", tmax=tmax), metaM("HewittHoenig", tmax=tmax), 
+      metaM("tmax1", tmax=tmax), metaM("K1", K=K), metaM("JensenK1", K=K),
+      metaM("AlversonCarney", tmax=tmax, K=K))
+v <- 1-exp(-M)
+ybh<-data.frame(name, M, v)
+
+#c - summary M and v
+mean <- c(mean(M), mean(v))
+SD <- c(sd(M), sd(v))
+min <- c(min(M), min(v))
+max<- c(max(M), max(v))
+ybh.summary<-data.frame(mean, SD, min, max)
+rownames(ybh.summary)<-c("M", "v")
+
+#d - one method, different studies
+#method: K1
+#M and v table
+Population <- c("Roscoff, France (M)", "Roscoff, France (F)", "Navarro River (inland), California",
+                "Navarro River (upstream), California", "Navarro River (estuary), California", "Ooster Schelde, Netherlands",
+                "Cheshire, UK", "Kiel Bay, Germany", "Kandalaksha Bay, Russia (M)", "Kandalaksha Bay, Russia (F)")
+K <- c(4.2, 2.4, 2.09, 1.77, 1.78, 2.32, 0.64, 1.79, 0.57, 0.67)
+M<- rep(0, 10)
+for(i in 1:length(K)){
+M[i]<-c(metaM("K1", K=K[i]))
+} #warning for first iteration: "K value seems unreasonable", related to Males in Roscoff, France
+v <-1-exp(-M)
+sticky<-data.frame(Population, K, M, v)
+#sumamry M and v
+mean <- c(mean(M), mean(v))
+SD <- c(sd(M), sd(v))
+min <- c(min(M), min(v))
+max<- c(max(M), max(v))
+sticky.summary<-data.frame(mean, SD, min, max)
+rownames(ybh.summary)<-c("M", "v")
+
+
+
+
